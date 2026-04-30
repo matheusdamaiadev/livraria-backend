30b7503b48b48d467391aa52c1d555473058eac8
matheusdamaiadev <matheusdamaia1@gmail.com>
2026-04-30 14:52:44 -0300

    livro serializer

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,5 +1,4 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
-
+from rest_framework import serializers
 from core.models import Livro
 from uploader.models import Image
 
@@ -7,8 +6,8 @@ from uploader.models import Image
 # =========================
 # CREATE / UPDATE
 # =========================
-class LivroSerializer(ModelSerializer):
-    capa = PrimaryKeyRelatedField(
+class LivroSerializer(serializers.ModelSerializer):
+    capa = serializers.PrimaryKeyRelatedField(
         queryset=Image.objects.all(),
         required=False,
         allow_null=True
@@ -16,19 +15,36 @@ class LivroSerializer(ModelSerializer):
 
     class Meta:
         model = Livro
-        fields = ('id', 'titulo', 'preco', 'categoria', 'editora', 'capa', 'autores')
+        fields = [
+            'id',
+            'titulo',
+            'isbn',
+            'quantidade',
+            'preco',
+            'categoria',
+            'editora',
+            'autores',
+            'capa',
+        ]
+
+    def create(self, validated_data):
+        autores = validated_data.pop('autores', [])
+
+        livro = Livro.objects.create(**validated_data)
+
+        if autores:
+            livro.autores.set(autores)
+
+        return livro
 
     def update(self, instance, validated_data):
-        # remove ManyToMany para tratar separadamente
         autores = validated_data.pop('autores', None)
 
-        # atualiza campos normais
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
 
         instance.save()
 
-        # atualiza ManyToMany corretamente
         if autores is not None:
             instance.autores.set(autores)
 
@@ -36,26 +52,26 @@ class LivroSerializer(ModelSerializer):
 
 
 # =========================
-# RETRIEVE (DETALHADO)
-# =========================
-class LivroRetrieveSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = '__all__'
-        depth = 1  # categorias, editora, autores com detalhes
-
-
-# =========================
 # LISTAGEM SIMPLES
 # =========================
-class LivroListSerializer(ModelSerializer):
+class LivroListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Livro
         fields = (
             'id',
             'titulo',
             'preco',
-            'capa',
             'categoria',
             'editora',
         )
+
+
+# =========================
+# DETALHE COMPLETO
+# =========================
+class LivroRetrieveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Livro
+        fields = '__all__'
